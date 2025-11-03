92f5ae010f2ff727e163d1d2c23a454d5283b83e
JM97142 <jeanmichel.taret@gmail.com>
2025-11-03 04:22:06 +0100

    test build/sonar 7.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,17 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("org.sonarqube") version "5.1.0.4882"
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "JM97142_P15-Eventorias")
+        property("sonar.organization", "jm97142")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "src/main/java,src/main/kotlin")
+        property("sonar.tests", "src/test/java,src/test/kotlin")
+        property("sonar.java.binaries", "${project.buildDir}/intermediates/javac/debug/classes")
+        property("sonar.kotlin.binaries", "${project.buildDir}/tmp/kotlin-classes/debug")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/jacoco/jacocoUnitTestReport/jacocoUnitTestReport.xml")
+        property("sonar.coverage.exclusions", "**/R.class,**/R$*.class,**/BuildConfig.*,**/Manifest*.*,**/*Test*.*")
+    }
+}
